191ac94b05f5c131368f918d55d55d47dd1582be
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-05-12 11:19:41 +0300

    Working on the navigation

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Navbar.css';
+import ReorderIcon from '@mui/icons-material/Reorder';
+function Navbar() {
+    return (
+        <div className='navbar'>
+            <div className='toggleButton'>
+                <button><ReorderIcon /></button>
+            </div>
+            <div className='links'>
+                <Link to="/"> Home </Link>
+                <Link to="/projects"> Projects </Link>
+                <Link to="/experience"> Experience </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/pages/Experience.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Experience() {
+  return (
+    <div>This is the experience page</div>
+  )
+}
+
+export default Experience
