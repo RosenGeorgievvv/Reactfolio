b096e33f769dbb48f1f051fd01e5e45f2f61fcd0
Rosen Georgiev <rosengeorgiew1@gmail.com>
2023-05-13 09:49:55 +0300

    Projects page ready

---
+++ src/components/ProjectList.jsx
@@ -0,0 +1,27 @@
+import calc from "../assets/calc.jpg";
+import expensesTracker from "../assets/expensesTracker.JPG"
+import movieland from "../assets/movieland.png";
+import todolist from "../assets/todolist.webp"
+
+export const ProjectList = [
+    {
+      name: "React Calculator",
+      image: calc,
+      skills: "JavaScript,HTML,CSS",
+    },
+    {
+      name: "Expenses Tracker",
+      image: expensesTracker,
+      skills: "React,Node.js,MongoDB",
+    },
+    {
+      name: "Movie Land",
+      image: movieland,
+      skills: "React,Node.js,MongoDB,SpotifyAPI",
+    },
+    {
+      name: "To Do List",
+      image: todolist,
+      skills: "React,Node.js,MySQL,GraphQL",
+    }
+  ];
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import ProjectItem from '../components/ProjectItem'
+import '../styles/Projects.css'
+import { ProjectList } from '../components/ProjectList'
+
 
 function Projects() {
   return (
     <div className='projects'>
       <h1>My Personal Projects</h1>
       <div className='projectList'>
-        <ProjectItem />
-        <ProjectItem />
+        {ProjectList.map((project) =>{
+          return <ProjectItem name={project.name} image={project.image} />
+        })}
       </div>
     </div>
   )
